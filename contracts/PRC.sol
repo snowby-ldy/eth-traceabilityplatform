bad11c2f4c0f0f204381a1fdcf476a21c10c2fc3
anarkrypto <anarkrypto@gmail.com>
2022-05-06 17:28:15 -0300

    update contracts and solidity

--- contracts/PRC.sol
+++ contracts/PRC.sol
@@ -1,137 +1,167 @@
-pragma solidity ^ 0.4 .10;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
 contract PRC {
+    // Mapping from user address to boolean type
+    mapping(address => bool) isAuthorized;
 
-  // Mapping from user address to boolean type
-  mapping(address => bool) isAuthorized;
+    // Define struct
+    struct product {
+        string _productName;
+        string _productCode;
+        string _rawMaterials;
+        address _productOwner;
+        uint256 _timestamp;
+        address _BACAddress;
+    }
 
-  // Define struct
-  struct product {
-    string _productName;
-    string _productCode;
-    string _rawMaterials;
-    address _productOwner;
-    uint _timestamp;
-    address _BACAddress;
-  }
+    mapping(uint256 => product) _products;
 
-  mapping(uint => product) _products;
+    mapping(string => uint256) _productCodeToId;
 
-  mapping(string => uint) _productCodeToId;
+    uint256 _numberOfProducts;
 
-  uint _numberOfProducts;
+    address payable _admin;
 
-  address _admin;
+    mapping(string => string) _productCodeToName;
+    mapping(string => address) _productCodeToBACAddress;
 
-  mapping(string => string) _productCodeToName;
-  mapping(string => address) _productCodeToBACAddress;
+    // As a prerequisite for some functions
+    modifier onlyAdmin() {
+        require(msg.sender == payable(_admin));
+        _;
+    }
 
-  // As a prerequisite for some functions
-  modifier onlyAdmin {
-    require(msg.sender == _admin);
-    _;
-  }
+    modifier onlyAuthorized(address addr) {
+        require(isAuthorized[addr] == true);
+        _;
+    }
 
-  modifier onlyAuthorized(address addr) {
-    require(isAuthorized[addr] == true);
-    _;
-  }
+    // Constructor function
+    constructor() {
+        _admin = payable(msg.sender);
+        _numberOfProducts = 1;
+    }
 
-  // Constructor function
-  constructor() public {
-    _admin = msg.sender;
-    _numberOfProducts = 1;
-  }
+    // Register product information
+    function productRegister(
+        string memory productName,
+        string memory productCode,
+        string memory rawMaterials,
+        address BACAddress
+    ) public onlyAuthorized(msg.sender) {
+        require(bytes(_productCodeToName[productCode]).length == 0);
 
-  // Register product information
-  function productRegister(string productName, string productCode, string rawMaterials, address BACAddress) public onlyAuthorized(msg.sender) {
+        require(
+            bytes(productName).length >= 3 && bytes(productName).length <= 64
+        );
+        require(bytes(productCode).length == 13);
+        require(bytes(rawMaterials).length >= 9);
 
-    require(bytes(_productCodeToName[productCode]).length == 0);
+        _productCodeToName[productCode] = productName;
+        _productCodeToId[productCode] = _numberOfProducts;
+        _productCodeToBACAddress[productCode] = BACAddress;
 
-    require(bytes(productName).length >= 3 && bytes(productName).length <= 64);
-    require(bytes(productCode).length == 13);
-    require(bytes(rawMaterials).length >= 9);
+        _products[_numberOfProducts]._productName = productName;
+        _products[_numberOfProducts]._productCode = productCode;
+        _products[_numberOfProducts]._rawMaterials = rawMaterials;
+        _products[_numberOfProducts]._productOwner = msg.sender;
+        _products[_numberOfProducts]._timestamp = block.timestamp;
+        _products[_numberOfProducts]._BACAddress = BACAddress;
 
-    _productCodeToName[productCode] = productName;
-    _productCodeToId[productCode] = _numberOfProducts;
-    _productCodeToBACAddress[productCode] = BACAddress;
+        _numberOfProducts++;
+    }
 
-    _products[_numberOfProducts]._productName = productName;
-    _products[_numberOfProducts]._productCode = productCode;
-    _products[_numberOfProducts]._rawMaterials = rawMaterials;
-    _products[_numberOfProducts]._productOwner = msg.sender;
-    _products[_numberOfProducts]._timestamp = now;
-    _products[_numberOfProducts]._BACAddress = BACAddress;
+    // Register raw material information
+    function materialRegister(
+        string memory materialName,
+        string memory materialCode,
+        address BACAddress
+    ) public onlyAuthorized(msg.sender) {
+        require(bytes(_productCodeToName[materialCode]).length == 0);
 
-    _numberOfProducts++;
+        require(
+            bytes(materialName).length >= 3 && bytes(materialName).length <= 64
+        );
+        require(bytes(materialCode).length == 9);
 
-  }
+        _productCodeToName[materialCode] = materialName;
+        _productCodeToId[materialCode] = _numberOfProducts;
+        _productCodeToBACAddress[materialCode] = BACAddress;
 
-  // Register raw material information
-  function materialRegister(string materialName, string materialCode, address BACAddress) public onlyAuthorized(msg.sender) {
+        _products[_numberOfProducts]._productName = materialName;
+        _products[_numberOfProducts]._productCode = materialCode;
+        _products[_numberOfProducts]._rawMaterials = "/";
+        _products[_numberOfProducts]._productOwner = msg.sender;
+        _products[_numberOfProducts]._timestamp = block.timestamp;
+        _products[_numberOfProducts]._BACAddress = BACAddress;
 
-    require(bytes(_productCodeToName[materialCode]).length == 0);
+        _numberOfProducts++;
+    }
 
-    require(bytes(materialName).length >= 3 && bytes(materialName).length <= 64);
-    require(bytes(materialCode).length == 9);
+    // Get the number of products
+    function getNumberOfProducts()
+        public
+        view
+        returns (uint256 numberOfProducts)
+    {
+        numberOfProducts = _numberOfProducts - 1;
+    }
 
-    _productCodeToName[materialCode] = materialName;
-    _productCodeToId[materialCode] = _numberOfProducts;
-    _productCodeToBACAddress[materialCode] = BACAddress;
+    // Get product information by id
+    function getProductOfId(uint256 id)
+        public
+        view
+        returns (
+            string memory productName,
+            string memory productCode,
+            string memory rawMaterials,
+            address productOwner,
+            uint256 timestamp,
+            address BACAddress
+        )
+    {
+        productName = _products[id]._productName;
+        productCode = _products[id]._productCode;
+        rawMaterials = _products[id]._rawMaterials;
+        productOwner = _products[id]._productOwner;
+        timestamp = _products[id]._timestamp;
+        BACAddress = _products[id]._BACAddress;
+    }
 
-    _products[_numberOfProducts]._productName = materialName;
-    _products[_numberOfProducts]._productCode = materialCode;
-    _products[_numberOfProducts]._rawMaterials = "/";
-    _products[_numberOfProducts]._productOwner = msg.sender;
-    _products[_numberOfProducts]._timestamp = now;
-    _products[_numberOfProducts]._BACAddress = BACAddress;
+    function getIdOfCode(string memory productCode)
+        public
+        view
+        returns (uint256 id)
+    {
+        id = _productCodeToId[productCode];
+    }
 
-    _numberOfProducts++;
+    function getAddressOfCode(string memory productCode)
+        public
+        view
+        returns (address addr)
+    {
+        addr = _productCodeToBACAddress[productCode];
+    }
 
-  }
+    // Add user to authorization list
+    function addUser(address addr) public onlyAdmin {
+        isAuthorized[addr] = true;
+    }
 
-  // Get the number of products
-  function getNumberOfProducts() constant public returns(uint numberOfProducts) {
-    numberOfProducts = _numberOfProducts - 1;
-  }
+    // Remove user
+    function removeUser(address addr) public onlyAdmin {
+        isAuthorized[addr] = false;
+    }
 
-  // Get product information by id
-  function getProductOfId(uint id) constant public returns(string productName, string productCode, string rawMaterials, address productOwner, uint timestamp, address BACAddress) {
+    // Check if the user is authorized
+    function checkUser(address addr) public view returns (bool result) {
+        result = isAuthorized[addr];
+    }
 
-    productName = _products[id]._productName;
-    productCode = _products[id]._productCode;
-    rawMaterials = _products[id]._rawMaterials;
-    productOwner = _products[id]._productOwner;
-    timestamp = _products[id]._timestamp;
-    BACAddress = _products[id]._BACAddress;
-  }
-
-  function getIdOfCode(string productCode) constant public returns(uint id) {
-    id = _productCodeToId[productCode];
-  }
-
-  function getAddressOfCode(string productCode) constant public returns(address addr) {
-    addr = _productCodeToBACAddress[productCode];
-  }
-
-  // Add user to authorization list
-  function addUser(address addr) public onlyAdmin {
-    isAuthorized[addr] = true;
-  }
-
-  // Remove user
-  function removeUser(address addr) public onlyAdmin {
-    isAuthorized[addr] = false;
-  }
-
-  // Check if the user is authorized
-  function checkUser(address addr) constant public returns(bool result) {
-    result = isAuthorized[addr];
-  }
-
-  // Destroy the contract
-  function deleteContract() public onlyAdmin {
-    selfdestruct(_admin);
-  }
-
+    // Destroy the contract
+    function deleteContract() public onlyAdmin {
+        selfdestruct(_admin);
+    }
 }
--- contracts/TUC.sol
+++ contracts/TUC.sol
@@ -1,87 +1,97 @@
-pragma solidity ^ 0.4 .10;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
 contract TUC {
+    // Mapping from user address to boolean type
+    mapping(address => bool) isAuthorized;
 
-  // Mapping from user address to boolean type
-  mapping(address => bool) isAuthorized;
+    // Define struct
+    struct tr {
+        string _currentTx;
+        string _previousTx;
+        address _sender;
+        string _receiver;
+        uint256 _time;
+    }
 
-  // Define struct
-  struct tr {
-    string _currentTx;
-    string _previousTx;
-    address _sender;
-    string _receiver;
-    uint _time;
-  }
+    mapping(uint256 => tr) trs;
 
-  mapping(uint => tr) trs;
+    uint256 _numberOfTrs;
 
-  uint _numberOfTrs;
+    address payable _batchAdmin;
 
-  address _batchAdmin;
+    // As a prerequisite for some functions
+    modifier onlyAdmin() {
+        require(msg.sender == _batchAdmin);
+        _;
+    }
 
-  // As a prerequisite for some functions
-  modifier onlyAdmin {
-    require(msg.sender == _batchAdmin);
-    _;
-  }
+    modifier onlyAuthorized(address addr) {
+        require(isAuthorized[addr] == true);
+        _;
+    }
 
-  modifier onlyAuthorized(address addr) {
-    require(isAuthorized[addr] == true);
-    _;
-  }
+    // Constructor function
+    constructor() {
+        _batchAdmin = payable(msg.sender);
+        _numberOfTrs = 1;
+    }
 
-  // Constructor function
-  constructor() public {
-    _batchAdmin = msg.sender;
-    _numberOfTrs = 1;
-  }
+    // Update transaction information
+    function addTr(
+        string memory currentTx,
+        string memory previousTx,
+        string memory re
+    ) public onlyAuthorized(msg.sender) {
+        trs[_numberOfTrs]._currentTx = currentTx;
+        trs[_numberOfTrs]._previousTx = previousTx;
+        trs[_numberOfTrs]._sender = msg.sender;
+        trs[_numberOfTrs]._receiver = re;
+        trs[_numberOfTrs]._time = block.timestamp;
 
-  // Update transaction information
-  function addTr(string currentTx, string previousTx, string re) public onlyAuthorized(msg.sender) {
+        _numberOfTrs++;
+    }
 
-    trs[_numberOfTrs]._currentTx = currentTx;
-    trs[_numberOfTrs]._previousTx = previousTx;
-    trs[_numberOfTrs]._sender = msg.sender;
-    trs[_numberOfTrs]._receiver = re;
-    trs[_numberOfTrs]._time = now;
+    // Get transaction information by id
+    function getTrOfId(uint256 id)
+        public view
+        returns (
+            string memory currentTx,
+            string memory previousTx,
+            address se,
+            string memory re,
+            uint256 time
+        )
+    {
+        currentTx = trs[id]._currentTx;
+        previousTx = trs[id]._previousTx;
+        se = trs[id]._sender;
+        re = trs[id]._receiver;
+        time = trs[id]._time;
+    }
 
-    _numberOfTrs++;
+    // Get the number of transactions
+    function getNumberOfTrs() public view returns (uint256 numberOfTrs) {
+        numberOfTrs = _numberOfTrs - 1;
+    }
 
-  }
+    // Add user to authorization list
+    function addUser(address addr) public onlyAdmin {
+        isAuthorized[addr] = true;
+    }
 
-  // Get transaction information by id
-  function getTrOfId(uint id) constant public returns(string currentTx, string previousTx, address se, string re, uint time) {
-    currentTx = trs[id]._currentTx;
-    previousTx = trs[id]._previousTx;
-    se = trs[id]._sender;
-    re = trs[id]._receiver;
-    time = trs[id]._time;
-  }
+    // Remove user
+    function removeUser(address addr) public onlyAdmin {
+        isAuthorized[addr] = false;
+    }
 
-  // Get the number of transactions
-  function getNumberOfTrs() constant public returns(uint numberOfTrs) {
-    numberOfTrs = _numberOfTrs - 1;
-  }
+    // Check if the user is authorized
+    function checkUser(address addr) public view returns (bool result) {
+        result = isAuthorized[addr];
+    }
 
-  // Add user to authorization list
-  function addUser(address addr) public onlyAdmin {
-    isAuthorized[addr] = true;
-  }
-
-  // Remove user
-  function removeUser(address addr) public onlyAdmin {
-    isAuthorized[addr] = false;
-  }
-
-  // Check if the user is authorized
-  function checkUser(address addr) constant public returns(bool result) {
-    result = isAuthorized[addr];
-  }
-
-  // Destroy the contract
-  function deleteContract() public onlyAdmin {
-    selfdestruct(_batchAdmin);
-  }
-
+    // Destroy the contract
+    function deleteContract() public onlyAdmin {
+        selfdestruct(_batchAdmin);
+    }
 }
